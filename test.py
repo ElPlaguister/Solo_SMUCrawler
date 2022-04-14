eee5b0b5e415b1e32d090a6d11530e79a50d9bcd
Kritias <50093609+ElPlaguister@users.noreply.github.com>
2022-04-14 16:52:42 +0900

    SMU Service

--- test.py
+++ test.py
@@ -29,22 +29,50 @@ def crawl_swai(session):
     res = [url.split('=')[-2].split('&')[0] for url in urls]
     return res
 
-def add_ids(ids):
-    f = open('private/data.txt', 'r')
+def crawl_smu(session):
+    endpoint = 'https://www.smu.ac.kr/lounge/notice/notice.do'
+    soup = bs(session.get(endpoint).text, 'html.parser')
+    tableText = soup.find('ul', {'class' : 'board-thumb-wrap'})
+    ids = [no_space(a.text.strip()).replace('No.', '') for a in tableText.findAll('li', {'class' : 'board-thumb-content-number'})]
+    return ids
+
+def add_swai_ids(ids):
+    f = open('private/data_swai.txt', 'r')
     datum = f.readline().split(' ')
     for id in ids:
         if id not in datum:
             datum.append(id)
     datum = ' '.join(datum)
     f.close()
-    f = open('private/data.txt', 'w')
+    f = open('private/data_swai.txt', 'w')
     f.write(datum)
     f.close()
 
-def check_ids(ids):
+def check_swai_ids(ids):
     datum = []
     res = []
-    with open('private/data.txt', 'r') as f:
+    with open('private/data_swai.txt', 'r') as f:
+        datum = f.readline().split(' ')
+    for id in ids:
+        if id not in datum:
+            res.append(id)
+    return res
+
+def add_smu_id(ids):
+    f = open('private/data_smu.txt', 'r')
+    datum = f.readline().split(' ')
+    for id in ids:
+        if id not in datum:
+            datum.append(id)
+    datum = ' '.join(datum)
+    f.close()
+    f = open('private/data_smu.txt', 'w')
+    f.write(datum)
+    f.close()
+
+def check_smu_id(ids):
+    res = []
+    with open('private/data_smu.txt', 'r') as f:
         datum = f.readline().split(' ')
     for id in ids:
         if id not in datum:
@@ -53,12 +81,51 @@ def check_ids(ids):
 
 def set_all_read():
     ids = crawl_swai()
-    add_ids(ids)
+    add_swai_ids(ids)
 
 def no_space(text):
   text1 = re.sub('\nbsp;|&nbsp;|\n|\t|\r|  ', '', text)
   text2 = re.sub('\n\n', '', text1)
   return text2
+
+def crawl_smu(session, set_all_read = False, ignore_cheon = True):
+    endpoint = 'https://www.smu.ac.kr/lounge/notice/notice.do'
+    soup = bs(session.get(endpoint).text, 'html.parser')
+    tableText = soup.find('ul', {'class' : 'board-thumb-wrap'})
+    contents = tableText.findAll('dl', {'class' : 'board-thumb-content-wrap'})
+    articles = []
+    ids = []
+    for content in contents:
+        tmp = content.find('dd', {'class' : 'board-thumb-content-info'})
+        id = no_space(tmp.find('li', {'class' : 'board-thumb-content-number'}).text.strip()).replace('No.', '')
+        date = re.findall(r'[0-9]+-[0-9]+-[0-9]+', tmp.find('li', {'class', 'board-thumb-content-date'}).text)[0]
+        region = content.find('span', {'class' : 'cmp'}).text
+        category = content.find('span', {'class' : 'cate'}).text
+        tmp_url = 'https://www.smu.ac.kr/lounge/notice/notice.do?mode=view&articleNo=' + id + '&article.offset=0&articleLimit=10'
+        title = no_space([a.text for a in content.find('dt', {'class' : 'board-thumb-content-title'}).findAll('a')][-1])
+        article = {
+            'region' : region,
+            'category' : category,
+            'url' : tmp_url,
+            'title' : title,
+            'date' : date,
+            'id' : id
+        }
+        articles.append(article)
+        ids.append(id)
+    ids = check_smu_id(ids)
+    if set_all_read:
+        add_smu_id(ids)
+        return
+    delList = []
+    for (i, article) in enumerate(articles):
+        if article['id'] not in ids:
+            delList.append(i)
+        elif ignore_cheon & (article['region'] == '천안'):
+            delList.append(i)
+    for i in delList:
+        articles[i] = None
+    return articles
 
 def get_content_swai(id, session):
     endpoint = 'https://swai.smu.ac.kr/bbs/board.php?bo_table=07_01&wr_id=' + str(id)
@@ -67,11 +134,11 @@ def get_content_swai(id, session):
 
     title = article.find('span', {'class' : 'bo_v_tit'}).text
     title = no_space(title).strip()
-    date = ' '.join(article.find('strong', {'class' : 'if_date'}).text.split(' ')[1:])
+    date = article.find('strong', {'class' : 'if_date'}).text.split(' ')[1]
     content = article.find('div', {'id' : 'bo_v_con'})
     res = {
         'region' : '서울',
-        'category' : 'SWAI',
+        'category' : '[SWAI]',
         'url' : endpoint,
         'title' : title,
         'date' : date,
@@ -82,17 +149,23 @@ def get_content_swai(id, session):
 def config_mail(contents):
     with open('mailFrame.html', 'r') as f:
         frame = ' '.join(f.readlines())
+    none_count = 0
     for i in range(len(contents)):
+        if contents[i] == None:
+            none_count += 1
+            continue
         with open('content.html', 'r') as f:
             c = ' '.join(f.readlines())
         c = c.replace('__region__', contents[i]['region'])
         c = c.replace('__url__', contents[i]['url'])
         c = c.replace('__title__', contents[i]['title'])
         c = c.replace('__date__', contents[i]['date'])
-        c = c.replace('__category__', '[' + contents[i]['category'] + ']')
-        if i + 1 != len(contents):
-            c += '__content__'
+        c = c.replace('__category__', contents[i]['category'])
+        c += '__content__'
         frame = frame.replace('__content__', c)
+    frame = frame.replace('__content__', '')
+    if none_count == len(contents):
+        frame = None
     return frame
 
 def send_mail(target, content, subject = '오늘의 사업단 글입니다.'):
@@ -105,7 +178,7 @@ def send_mail(target, content, subject = '오늘의 사업단 글입니다.'):
 
     msg = MIMEText(content, 'html')
     msg['Subject'] = subject
-    mail_session.sendmail("Secretary", target, msg.as_string())
+    mail_session.sendmail("Secretary@gmail.com", target, msg.as_string())
 
 if __name__ == '__main__':
     with open('private/target.txt', 'r') as f:
@@ -115,5 +188,8 @@ if __name__ == '__main__':
     contents = []
     for id in ids:
         contents.append(get_content_swai(id, session))
+    contents.extend(crawl_smu(session))
     mail_body = config_mail(contents)
-    send_mail(targets[0], mail_body, subject = '오늘의 학교 정보입니다.')
+
+    if mail_body != None:
+        send_mail(targets[0], mail_body, subject = '오늘의 학교 정보입니다.')
