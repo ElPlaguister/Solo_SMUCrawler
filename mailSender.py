208d7e75240ba21a5ab8632d0e1e66f3e9d2aaa7
kritias <50093609+ElPlaguister@users.noreply.github.com>
2022-06-18 18:25:03 +0900

    Feat : Add Exception Capturer

--- mailSender.py
+++ mailSender.py
@@ -50,31 +50,33 @@ def crawl_swai(session):
         except:
             print('crawl swai failed')
             return []
+        try:
+            tableText = soup.find('div', {'class' : 'tbl_head01 tbl_wrap'}).find('tbody')
+            urls = [a['href'] for a in tableText.findAll('a')]
+            id = [x for x in (url.split('=')[-2].split('&')[0] for url in urls)]
+            title = [no_space(t.text) for t in tableText.findAll('a')]
+            date = [t.text for t in tableText.findAll('td', {'class' : 'td_datetime'})]
 
-        tableText = soup.find('div', {'class' : 'tbl_head01 tbl_wrap'}).find('tbody')
-        urls = [a['href'] for a in tableText.findAll('a')]
-        id = [x for x in (url.split('=')[-2].split('&')[0] for url in urls)]
-        title = [no_space(t.text) for t in tableText.findAll('a')]
-        date = [t.text for t in tableText.findAll('td', {'class' : 'td_datetime'})]
-
-        ids.extend(id)
-        for i in range(len(id)):
-            res.append({
-                'region' : '대학',
-                'category' : '[SWAI]',
-                'url' : urls[i],
-                'title' : title[i],
-                'date' : date[i],
-                'id' : id[i]
-            })
-    ids = check_id(ids, 'swai')
-    delList = []
-    for (i, article) in enumerate(res):
-        if article['id'] not in ids:
-            delList.append(i)
-    for i in delList:
-        res[i] = None
-    return res
+            ids.extend(id)
+            for i in range(len(id)):
+                res.append({
+                    'region' : '대학',
+                    'category' : '[SWAI]',
+                    'url' : urls[i],
+                    'title' : title[i],
+                    'date' : date[i],
+                    'id' : id[i]
+                })
+        except:
+            return []
+        ids = check_id(ids, 'swai')
+        delList = []
+        for (i, article) in enumerate(res):
+            if article['id'] not in ids:
+                delList.append(i)
+        for i in delList:
+            res[i] = None
+        return res
 
 def crawl_smu(session, ignore_cheon = True):
     endpoint = 'https://www.smu.ac.kr/lounge/notice/notice.do'
@@ -83,38 +85,41 @@ def crawl_smu(session, ignore_cheon = True):
     except:
         print('crawl smu failed')
         return []
-    tableText = soup.find('ul', {'class' : 'board-thumb-wrap'})
-    contents = tableText.findAll('dl', {'class' : 'board-thumb-content-wrap'})
-    articles = []
-    ids = []
-    for content in contents:
-        tmp = content.find('dd', {'class' : 'board-thumb-content-info'})
-        id = no_space(tmp.find('li', {'class' : 'board-thumb-content-number'}).text.strip()).replace('No.', '')
-        date = re.findall(r'[0-9]+-[0-9]+-[0-9]+', tmp.find('li', {'class', 'board-thumb-content-date'}).text)[0]
-        region = content.find('span', {'class' : 'cmp'}).text
-        category = content.find('span', {'class' : 'cate'}).text
-        tmp_url = 'https://www.smu.ac.kr/lounge/notice/notice.do?mode=view&articleNo=' + id + '&article.offset=0&articleLimit=10'
-        title = no_space([a.text for a in content.find('dt', {'class' : 'board-thumb-content-title'}).findAll('a')][-1])
-        article = {
-            'region' : '대학',
-            'category' : category,
-            'url' : tmp_url,
-            'title' : title,
-            'date' : date,
-            'id' : id
-        }
-        articles.append(article)
-        ids.append(id)
-    ids = check_id(ids, 'smu')
-    delList = []
-    for (i, article) in enumerate(articles):
-        if article['id'] not in ids:
-            delList.append(i)
-        elif ignore_cheon & (article['region'] == '천안'):
-            delList.append(i)
-    for i in delList:
-        articles[i] = None
-    return articles
+    try:
+        tableText = soup.find('ul', {'class' : 'board-thumb-wrap'})
+        contents = tableText.findAll('dl', {'class' : 'board-thumb-content-wrap'})
+        articles = []
+        ids = []
+        for content in contents:
+            tmp = content.find('dd', {'class' : 'board-thumb-content-info'})
+            id = no_space(tmp.find('li', {'class' : 'board-thumb-content-number'}).text.strip()).replace('No.', '')
+            date = re.findall(r'[0-9]+-[0-9]+-[0-9]+', tmp.find('li', {'class', 'board-thumb-content-date'}).text)[0]
+            region = content.find('span', {'class' : 'cmp'}).text
+            category = content.find('span', {'class' : 'cate'}).text
+            tmp_url = 'https://www.smu.ac.kr/lounge/notice/notice.do?mode=view&articleNo=' + id + '&article.offset=0&articleLimit=10'
+            title = no_space([a.text for a in content.find('dt', {'class' : 'board-thumb-content-title'}).findAll('a')][-1])
+            article = {
+                'region' : '대학',
+                'category' : category,
+                'url' : tmp_url,
+                'title' : title,
+                'date' : date,
+                'id' : id
+            }
+            articles.append(article)
+            ids.append(id)
+        ids = check_id(ids, 'smu')
+        delList = []
+        for (i, article) in enumerate(articles):
+            if article['id'] not in ids:
+                delList.append(i)
+            elif ignore_cheon & (article['region'] == '천안'):
+                delList.append(i)
+        for i in delList:
+            articles[i] = None
+        return articles
+    except:
+        return []
 
 def config_mail(contents):
     with open('mailFrame.html', 'r') as f:
